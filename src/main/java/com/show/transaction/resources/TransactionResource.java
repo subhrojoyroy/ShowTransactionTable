1059b785cc26185380e4129529a2644318589cd6
Subhrojoy Roy <subhrojoy_roy@yahoo.com>
2021-06-18 23:44:44 +0530

    Setting 'No content' response for no data

--- src/main/java/com/show/transaction/resources/TransactionResource.java
+++ src/main/java/com/show/transaction/resources/TransactionResource.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 @RestController
